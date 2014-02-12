b77b3524a2b73a2b411c33c95eab144499c24d03
Jace Ju <jaceju@gmail.com>
2014-02-12 17:59:01 +0800

    Add flot examples, part 2.

--- js/sb-flot-module.js
+++ js/sb-flot-module.js
@@ -52,4 +52,49 @@ angular.module('sbFlotModule', []).
                 };
             }
         };
+    }).
+    directive('sbFlotPieChart', function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            compile: function (element) {
+                element.replaceWith($('<div class="flot-chart-content"></div>'));
+
+                return function (scope, element) {
+                    var data = [{
+                        label: "Series 0",
+                        data: 1
+                    }, {
+                        label: "Series 1",
+                        data: 3
+                    }, {
+                        label: "Series 2",
+                        data: 9
+                    }, {
+                        label: "Series 3",
+                        data: 20
+                    }];
+
+                    $.plot(element, data, {
+                        series: {
+                            pie: {
+                                show: true
+                            }
+                        },
+                        grid: {
+                            hoverable: true
+                        },
+                        tooltip: true,
+                        tooltipOpts: {
+                            content: "%p.0%, %s", // show percentages, rounding to 2 decimal places
+                            shifts: {
+                                x: 20,
+                                y: 0
+                            },
+                            defaultTheme: false
+                        }
+                    });
+                };
+            }
+        };
     });
