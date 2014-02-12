d88371d17a0342ca1c967ae8b3e7a663dda92bad
Jace Ju <jaceju@gmail.com>
2014-02-12 14:12:50 +0800

    Add example templates.

--- js/app.js
+++ js/app.js
@@ -3,5 +3,38 @@
 var angular = window.angular;
 
 (function () {
-    angular.module('sbAdminApp', ['sbAdminModule']);
+    var app = angular.module('sbAdminApp', ['sbAdminModule', 'ngRoute']);
+    app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: '../partial/dashboard.html'
+            }).
+            when('/charts/flot', {
+                templateUrl: '../partial/flot.html'
+            }).
+            when('/charts/morris', {
+                templateUrl: '../partial/dashboard.html'
+            }).
+            when('/tables', {
+                templateUrl: '../partial/tables.html'
+            }).
+            when('/forms', {
+                templateUrl: '../partial/forms.html'
+            }).
+            when('/ui-elements/panels-wells', {
+                templateUrl: '../partial/panels-wells.html'
+            }).
+            when('/ui-elements/buttons', {
+                templateUrl: '../partial/buttons.html'
+            }).
+            when('/ui-elements/notifications', {
+                templateUrl: '../partial/notifications.html'
+            }).
+            when('/ui-elements/typography', {
+                templateUrl: '../partial/typography.html'
+            }).
+            when('/ui-elements/grid', {
+                templateUrl: '../partial/grid.html'
+            });
+    }]);
 })();
