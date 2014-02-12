c89d89bf7ac7ac292168cb77c8b4acd6126d8cc4
Jace Ju <jaceju@gmail.com>
2014-02-12 16:16:12 +0800

    Add morris.js example.

--- js/sb-admin.js
+++ js/sb-admin.js
@@ -4,28 +4,14 @@ $(function() {
 
 });
 
-//Loads the correct sidebar on window load
 $(function() {
 
-    $(window).bind("load", function() {
-        console.log($(this).width())
-        if ($(this).width() < 768) {
+    $(window).bind("load resize", function() {
+        console.log($(this).width());
+        if ($(this).width() < 753) {
             $('div.sidebar-collapse').addClass('collapse')
         } else {
             $('div.sidebar-collapse').removeClass('collapse')
         }
-    })
-})
-
-//Collapses the sidebar on window resize
-$(function() {
-
-    $(window).bind("resize", function() {
-        console.log($(this).width())
-        if ($(this).width() < 768) {
-            $('div.sidebar-collapse').addClass('collapse')
-        } else {
-            $('div.sidebar-collapse').removeClass('collapse')
-        }
-    })
-})
+    });
+});
