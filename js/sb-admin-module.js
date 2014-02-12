54c2148ccdf84d6e063ebae3675a449a3b9c6093
Jace Ju <jaceju@gmail.com>
2014-02-12 12:05:04 +0800

    Rename js file.

--- js/sb-admin-module.js
+++ js/sb-admin-module.js
@@ -1,58 +1,70 @@
-angular.module('sbAdminDirectives', []).
+angular.module('sbAdminModule', []).
     directive('sbTopNav', function() {
         return {
-            restrict: "E",
+            restrict: 'E',
             replace: true,
             scope: {
-                title: "="
+                title: '='
             },
             templateUrl: '../partial/top-nav.html'
         };
     }).
     directive('sbTopNavMessages', function() {
         return {
-            restrict: "E",
+            restrict: 'E',
             replace: true,
             templateUrl: '../partial/top-nav-messages.html'
         };
     }).
     directive('sbTopNavTasks', function() {
         return {
-            restrict: "E",
+            restrict: 'E',
             replace: true,
             templateUrl: '../partial/top-nav-tasks.html'
         };
     }).
     directive('sbTopNavAlerts', function() {
         return {
-            restrict: "E",
+            restrict: 'E',
             replace: true,
             templateUrl: '../partial/top-nav-alerts.html'
         };
     }).
     directive('sbTopNavUser', function() {
         return {
-            restrict: "E",
+            restrict: 'E',
             replace: true,
             templateUrl: '../partial/top-nav-user.html'
         };
     }).
     directive('sbSidebar', function () {
         return {
-            restrict: "E",
+            restrict: 'E',
             replace: true,
             templateUrl: '../partial/sidebar.html'
         };
     }).
-    directive('sbMetisMenu', function () {
+    directive('sbSidebarSearch', function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: '../partial/sidebar-search.html'
+        };
+    }).
+    directive('sbSidebarMenus', ['$http', '$timeout', function ($http, $timeout) {
         return {
             restrict: 'A',
             replace: false,
             transclude: false,
-            compile: function (element, attrs) {
-                return function (scope, element, attrs, controller) {
-                    element.metisMenu();
-                };
+            link: function (scope, element) {
+                scope.menus = [];
+                $http.get('/examples/json/menus.json').success(function (data) {
+                    scope.menus = data;
+
+                    $timeout(function () {
+                        element.metisMenu();
+                    });
+                });
             }
         };
-    });
+    }]);
